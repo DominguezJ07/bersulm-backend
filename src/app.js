0790124a8eee96f6d989a3fa4e6fa109bacfb193
DominguezJ07 <juliandominguezsena0908@gmail.com>
2026-06-18 16:53:54 -0500

    CAMBIOS REALIZADOS EN EL BACKEND

--- src/app.js
+++ src/app.js
@@ -48,8 +48,8 @@ app.use(
     }
   })
 );
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(sanitize);
 app.use(
